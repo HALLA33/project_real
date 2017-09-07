37ee4806c62ff38ae2d2eac9463e6ea37b8de354
ruddhr6963 <31616751+ruddhr6963@users.noreply.github.com>
2017-09-07 21:34:03 +0900

    Update BoardController.java
    도서 게시판 기능 추가, 페이징 추가

--- review/src/main/java/spring/controller/board/BoardController.java
+++ review/src/main/java/spring/controller/board/BoardController.java
@@ -1,6 +1,7 @@
 package spring.controller.board;
 
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -11,6 +12,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -36,90 +38,102 @@ public class BoardController {
 	}
 	
 	@RequestMapping("/list")
-	public String list(HttpServletRequest req) {
-//		List<Board> list = boardDao.list();
-//		req.setAttribute("list", list);
-		log.info("함수 실행");
-		return "board/list";
+	public String list(Model model, HttpServletRequest request, @RequestParam(required=false) int item) {
+		String pageStr = request.getParameter("page");
+		int pageNo;
+		
+		try{
+			pageNo = Integer.parseInt(pageStr);
+			if(pageNo <= 0) throw new Exception();
+		} catch(Exception e){
+			pageNo = 1;
+		}
+		
+		int boardSize = 10;
+		int boardCount = boardDao.count(item); 
+		
+		int start = boardSize * pageNo - boardSize + 1;
+		int end = start + boardSize - 1;
+		if(end > boardCount)
+			end = boardCount;
+		
+		int blockSize = 10;
+		int blockTotal = (boardCount + boardSize-1) / boardSize;
+		int startBlock = (pageNo - 1) / blockSize * blockSize + 1;
+		int endBlock = startBlock + blockSize - 1;
+		
+		if(blockTotal<endBlock)
+			endBlock = blockTotal;
+		
+		String url = "list?a=1?item="+item;
+		
+		List<Board> board = boardDao.board_list(start, end, item);
+		
+		Map<Integer, Book> book = null;
+		for(Board b: board) {
+			book = boardDao.book_list(b.getSearch_no());
+		}
+		
+		model.addAttribute("board", board);
+		model.addAttribute("book", book);
+		model.addAttribute("startBlock", startBlock);
+		model.addAttribute("endBlock", endBlock);
+		model.addAttribute("blockTotal", blockTotal);
+		model.addAttribute("boardCount", boardCount);
+		model.addAttribute("url", url);
+		model.addAttribute("pageNo", pageNo);
+		
+		return "board/list"; 
 	}
 	
 	@RequestMapping("/book-write")
-	public String search_book(Model model, @RequestParam(required=false) String image, @RequestParam(required=false) String title, @RequestParam(required=false) String author, @RequestParam(required=false) String publisher, @RequestParam(required=false) String pubdate) {
-		log.info("image : " + image);
-    	log.info("title : " + title);
-    	log.info("author : " + author);
-    	log.info("publisher : " + publisher);
-    	log.info("pubdate : " + pubdate);
-    	
-    	Book book = new Book();
-    	
-    	model.addAttribute("name", title);
-    	
-    	if(image==null)
-    		image = "http://placehold.it/120x120";
-    	if(title==null)
-    		title = "책 제목";
-    	if(author==null)
-    		author = "저자";
-    	if(publisher==null)
-    		publisher = "출판사";
-    	if(pubdate==null)
-    		pubdate = "출판일";
-
-    	book.setImage(image);
-    	book.setTitle(title);
-    	book.setAuthor(author);
-    	book.setPublisher(publisher);
-    	book.setPubdate(pubdate);
-    	
-    	model.addAttribute("search_book", book);
-    	
-    	return "board/book-write";
-    }
-	
-	@RequestMapping(value =  {"/movie-write", "/show-write"}, method=RequestMethod.GET)
-	public String write_get(HttpServletRequest request,HttpSession session) {
-		session.getAttribute("nickname");
-		
-		String servletPath  = (String)request.getServletPath();
-		if(servletPath.equals("/movie-write")) return "board/movie-write";
-		else if(servletPath.equals("/show-write")) return "/show-write";
-		else return "/";
+	public String book_write(){
+		return "board/book-write";
 	}
 	
-	@RequestMapping(value= {"/book-write", "/movie-write", "/show-write"}, method=RequestMethod.POST)
-	public String write_post(HttpServletRequest request) {
+	@RequestMapping(value= {"/book-write/{mode}"}, method=RequestMethod.POST)
+	public String write_post(Model model, HttpServletRequest request, @PathVariable String mode) {
 		Book book = new Book();
-		book.setImage(request.getParameter("p_image"));
-		book.setTitle(request.getParameter("p_title"));
-		book.setAuthor(request.getParameter("p_author"));
-		book.setPublisher(request.getParameter("p_publisher"));
-		book.setPubdate(request.getParameter("p_pubdate"));
+		book.setImage(request.getParameter("image"));
+		book.setTitle(request.getParameter("book_title"));
+		book.setAuthor(request.getParameter("author"));
+		book.setPublisher(request.getParameter("publisher"));
+		book.setPubdate(request.getParameter("pubdate"));
 		log.info("book : " + book.toString());
 		
-		Board b = new Board();
-		b.setItem_no(Integer.parseInt(request.getParameter("item_no")));
-		b.setHead(Integer.parseInt(request.getParameter("head")));
-		b.setWriter(request.getParameter("writer"));
-		b.setTitle(request.getParameter("title"));
-		b.setDetail(request.getParameter("detail"));
-		log.info("board : " + b.toString());
+		Board board = new Board();
+		board.setItem_no(Integer.parseInt(request.getParameter("item_no")));
+		board.setB_item_no(board.getItem_no());
+		board.setHead(Integer.parseInt(request.getParameter("head")));
+		board.setB_head(board.getHead());
+		board.setWriter(request.getParameter("writer"));
+		board.setTitle(request.getParameter("title"));
+		board.setDetail(request.getParameter("ir1"));
+		log.info("book-write board : " + board.toString());
 		
 		//String notice = request.getParameter("notice");
 		String notice = "false";
 		notice = (notice.equals("true")) ? "true" : "false";
 		
-		b.setNotice(notice);
+		board.setNotice(notice);
 		
 		String tag = request.getParameter("tag");
-		//tag.replaceAll(" ", "");//공백제거
-		//tag.replaceAll(",", "#");
-		b.setTag(tag);
+		if(tag!=null) {
+			tag.replaceAll(" ", "");//공백제거
+			tag.replaceAll(",", "#");
+		}
+		board.setTag(tag);
 		
-		int no = boardDao.search_write(book);
-		log.info("no : " + no);
-		boardDao.write(b, no);
-		return "board/list";
+		if(mode.equals("write")) {
+			int no = boardDao.search_write(book);
+			boardDao.write(board, no);
+		}
+			
+		model.addAttribute("mode", mode);
+		model.addAttribute("book", book);
+		model.addAttribute("board", board);
+		
+		return "board/book-detail";
 	}
 	
 	//키워드가 있을때도 있고 없을때도있음 
@@ -134,4 +148,53 @@ public class BoardController {
         
         return "board/bookList";
     }
+    
+    @RequestMapping("/book-revise")
+    public String bookRevise(Model model, HttpServletRequest request) {
+    	Book book = new Book();
+		book.setImage(request.getParameter("image"));
+		book.setTitle(request.getParameter("book_title"));
+		book.setAuthor(request.getParameter("author"));
+		book.setPublisher(request.getParameter("publisher"));
+		book.setPubdate(request.getParameter("pubdate"));
+		log.info("수정 : " + book.toString());
+		
+		Board board = new Board();
+		board.setItem_no(Integer.parseInt(request.getParameter("item_no")));
+		board.setB_item_no(board.getItem_no());
+		board.setHead(Integer.parseInt(request.getParameter("head")));
+		board.setB_head(board.getHead());
+		board.setWriter(request.getParameter("writer"));
+		board.setTitle(request.getParameter("title"));
+		board.setDetail(request.getParameter("detail"));
+		log.info("수정 : " + board.toString());
+		
+		//String notice = request.getParameter("notice");
+		String notice = "false";
+		notice = (notice.equals("true")) ? "true" : "false";
+		
+		board.setNotice(notice);
+		
+		String tag = request.getParameter("tag");
+		if(tag!=null) {
+			tag.replaceAll(" ", "");//공백제거
+			tag.replaceAll(",", "#");
+		}
+		board.setTag(tag);
+			
+		model.addAttribute("book", book);
+		model.addAttribute("board", board);
+    	
+    	return "board/book-revise";
+    }
+    
+    @RequestMapping(value =  {"/movie-write", "/show-write"}, method=RequestMethod.GET)
+	public String write_get(HttpServletRequest request,HttpSession session) {
+		session.getAttribute("nickname");
+		
+		String servletPath  = (String)request.getServletPath();
+		if(servletPath.equals("/movie-write")) return "board/movie-write";
+		else if(servletPath.equals("/show-write")) return "/show-write";
+		else return "/";
+	}
 }
