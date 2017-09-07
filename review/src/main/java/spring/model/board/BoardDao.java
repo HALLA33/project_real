d2e77ebde22aa5092ee9740635013f454c5d8393
ruddhr6963 <31616751+ruddhr6963@users.noreply.github.com>
2017-09-07 21:36:18 +0900

    Update BoardDao.java

--- review/src/main/java/spring/model/board/BoardDao.java
+++ review/src/main/java/spring/model/board/BoardDao.java
@@ -1,12 +1,15 @@
 package spring.model.board;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BoardDao {
-	List<Board> list();
+	List<Board> board_list(int start, int end, int item_no);
+	Map<Integer, Book> book_list(int no);
 	void write(Board board, int no);
 	int search_write(Book book);
+	int count(int item);
 }
