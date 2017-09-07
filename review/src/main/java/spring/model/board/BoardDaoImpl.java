e49bb55c42a0087e0e7e5d89437bd9a470918651
ruddhr6963 <31616751+ruddhr6963@users.noreply.github.com>
2017-09-07 21:37:56 +0900

    페이징처리와 도서게시판 리스트, 글쓰기, 검색한 데이터 디비에 저장하는 메소드 추가

--- review/src/main/java/spring/model/board/BoardDaoImpl.java
+++ review/src/main/java/spring/model/board/BoardDaoImpl.java
@@ -1,7 +1,8 @@
 package spring.model.board;
 
-import java.sql.ResultSet;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,6 +14,7 @@ import org.springframework.stereotype.Repository;
 @Repository("boardDao")
 public class BoardDaoImpl implements BoardDao{
 	private Logger log = LoggerFactory.getLogger(getClass());
+	private Map<Integer, Book> map = new HashMap<>();
 	
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
@@ -21,21 +23,32 @@ public class BoardDaoImpl implements BoardDao{
 		return new Board(rs);
 	};
 	
+	private RowMapper<Book> bmapper = (rs, index)->{
+		return new Book(rs);
+	};
+	
 	@Override
-	   public List<Board> list() {
-	      RowMapper<Board> mapper = (rs, index)->{
-	         Board board = new Board();
-	         board.setNo(rs.getInt("no"));
-	         board.setItem_no(rs.getInt("item_no"));
-	         board.setHead(rs.getInt("head"));
-	         board.setWriter(rs.getString("writer"));
-	         board.setTitle(rs.getString("title"));
-	         board.setReg(rs.getString("reg"));
-	         board.setRead(rs.getInt("read"));
-	         return board;
-	      };
-	      return jdbcTemplate.query("select * from p_board order by no desc", mapper);
-	   }
+	public List<Board> board_list(int start, int end, int item_no) {
+		String sql = "select * from "
+						+ "(select rownum rn, TMP.* from "
+						+ "(select * from p_board where item_no=?) TMP) "
+						+ "where rn between ? and ?";
+		
+		Object[] args = {start, end, item_no};
+		
+	   return jdbcTemplate.query(sql, args, mapper);
+	}
+	
+	@Override
+	public Map<Integer, Book> book_list(int no) {
+		Object[] args = {no};
+		List<Book> list = jdbcTemplate.query("select * from p_search where no=?", args, bmapper);
+		
+		Book book = list.get(0);
+		map.put(no, book);
+
+		return map;
+	}
 
 	public int search_no() {
 		String sql = "select p_search_seq.nextval from dual";
@@ -54,7 +67,15 @@ public class BoardDaoImpl implements BoardDao{
 		log.info("search_write실행", book.toString());
 		jdbcTemplate.update(sql, args);
 		
-		log.info("search_write에서 no : " + no);
+		return no;
+	}
+	
+	public int seq_number(String seq) {
+		seq += ".nextval";
+		String sql = "select " + seq + " from dual";
+		
+		int no = jdbcTemplate.queryForObject(sql, Integer.class);
+		
 		return no;
 	}
 	
@@ -74,15 +95,23 @@ public class BoardDaoImpl implements BoardDao{
 		case 7 : seq = "p_board_free_seq"; break;
 		}
 		
+		int seq_number = seq_number(seq);
+		
 		Object[] args = {
-				seq+".nextval", board.getItem_no(), board.getHead(), board.getTag(), 
+				seq_number, board.getItem_no(), board.getHead(), board.getTag(), 
 				board.getWriter(), board.getTitle(), board.getDetail(), board.getNotice(), no
 				};
 		
-		log.info("board 디비 추가 후: " + board.toString());
 		jdbcTemplate.update(sql, args);
 	}
 
-	
+	@Override
+	public int count(int item) {		
+		String sql = "select count(*) from p_board where item_no=?";
+		Object[] args = {item};
+		
+		int count = jdbcTemplate.queryForObject(sql, args, Integer.class);
+		return count;
+	}
 
 }
