54a1715523f2382d1e5aa4eb0c07a55f1cb82ce9
ruddhr6963 <31616751+ruddhr6963@users.noreply.github.com>
2017-09-11 18:59:13 +0900

    Update BoardDao.java

--- review/src/main/java/spring/model/board/BoardDao.java
+++ review/src/main/java/spring/model/board/BoardDao.java
@@ -9,7 +9,14 @@ import org.springframework.stereotype.Repository;
 public interface BoardDao {
 	List<Board> board_list(int start, int end, int item_no);
 	Map<Integer, Book> book_list(int no);
-	void write(Board board, int no);
+	int write(Board board, int no);
 	int search_write(Book book);
-	int count(int item);
+	String search_nickname(String id);
+	List<Book> exist_book(Book book);
+	int count(int item_no);
+	Book detail_book(int no);
+	Board detail_board(int no, int item_no, String writer);
+	Board detail_board(int no, int item_no);
+	void update_board(Board board, Book book, int no, int item_no, String writer);
+	void delete_board(int no, int item_no, String id);
 }
