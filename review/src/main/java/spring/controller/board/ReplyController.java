cbacf1890b3976eb50d40c5b3bee828a4a7f64dd
ruddhr6963 <31616751+ruddhr6963@users.noreply.github.com>
2017-09-25 15:40:20 +0900

    댓글 컨트롤러 추가

---
+++ review/src/main/java/spring/controller/board/ReplyController.java
@@ -0,0 +1,95 @@
+package spring.controller.board;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import spring.model.board.BookDao;
+import spring.model.board.Reply;
+import spring.model.board.ReplyDao;
+import spring.model.member.Member;
+
+@Controller
+@RequestMapping("/reply")
+public class ReplyController {
+	private Logger log = LoggerFactory.getLogger(getClass());
+	
+	@Autowired
+	private ReplyDao replyDao;
+	
+	@Autowired
+	private BookDao bookDao;
+	
+	@RequestMapping("/reply-insert")
+	@ResponseBody
+	public Reply insert(HttpServletRequest request) {
+		Reply reply = new Reply(request);
+		log.info("reply : " + reply.toString());
+
+		String ninkname = bookDao.search_nickname(reply.getWriter());
+		Reply r = replyDao.reply_insert(reply);
+		r.setNinkname(ninkname);
+		
+		return r;
+	}
+	
+	@RequestMapping("/reply-response-insert")
+	@ResponseBody
+	public Reply response_insert(HttpServletRequest request) {
+		Reply reply = new Reply();
+		reply.setWriter(request.getParameter("writer"));
+		reply.setDetail(request.getParameter("detail"));
+		reply.setBoard_no(Integer.parseInt(request.getParameter("board_no")));
+		reply.setBoard_item_no(Integer.parseInt(request.getParameter("board_item_no")));
+		reply.setNo(Integer.parseInt(request.getParameter("reply_no")));
+		
+		log.info("reply : " + reply.toString());
+		
+		String ninkname = bookDao.search_nickname(reply.getWriter());
+		Reply r = replyDao.reply_detail(reply.getNo());
+		reply.setGno(r.getGno());
+		reply.setGseq(r.getGseq());
+		reply.setDepth(r.getDepth());
+		log.info("gno, gseq, depth : " + r.toString());
+		log.info("gno, gseq, depth : " + reply.toString());
+		Reply re = replyDao.reply_insert(reply);
+		re.setNinkname(ninkname);
+		
+		return re;
+	}
+	
+	@RequestMapping("/reply-delete")
+	@ResponseBody
+	public boolean reply_delete(@RequestParam(required=false) int no, HttpSession session) {
+		Member member =(Member)session.getAttribute("member");
+		Reply reply = replyDao.reply_detail(no);
+		
+		boolean flag = true;
+		String msg = null;
+		log.info(member.getId());
+		log.info(reply.getWriter());
+		if(!member.getId().equals(reply.getWriter())) {
+			msg = "작성자가 아닙니다";
+			flag = false;
+			return flag;
+		}
+		else {
+			replyDao.reply_delete(no);
+			msg = "성공적으로 삭제되었습니다.";
+		}
+		
+		log.info("msg : " + msg);
+		return flag;
+	}
+}
