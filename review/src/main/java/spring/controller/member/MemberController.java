406af6fb8f2b64c5e1c3fe0309d5356f84f6cc36
multist <multist@DESKTOP-AGN29OT>
2017-09-28 23:59:48 +0900

    movie, etc, free 디테일에서 추천게시판, 내가쓴글, 내가쓴댓글 맵핑

--- review/src/main/java/spring/controller/member/MemberController.java
+++ review/src/main/java/spring/controller/member/MemberController.java
@@ -601,7 +601,7 @@ public class MemberController {
 	}
 
 	// 내가 쓴 글
-	@RequestMapping(value= {"/mycomment", "/movie/mycomment"})
+	@RequestMapping(value= {"/mycomment", "/movie/mycomment", "/etc/mycomment", "/free/mycomment"})
 	public String mycomment(HttpSession session, HttpServletRequest request,
 			@RequestParam(value = "mode", defaultValue = "new", required = false) String mode) {
 
@@ -616,7 +616,7 @@ public class MemberController {
 		return "member/mycomment";
 	}
 
-	@RequestMapping(value = {"/myboard", "/movie/myboard"})
+	@RequestMapping(value = {"/myboard", "/movie/myboard", "/etc/myboard", "/free/myboard"})
 	public String myboard(HttpSession session, HttpServletRequest request) {
 
 		Member member = (Member) session.getAttribute("member");
